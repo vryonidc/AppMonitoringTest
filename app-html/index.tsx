fd60851317a5455bf08a42e00ec352f136244ab6
Constantinos Vryonides <vryonidc@amazon.com>
2020-05-28 13:43:54 +0100

    sentry in webpack

--- app-html/index.tsx
+++ app-html/index.tsx
@@ -2,9 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { version } from "../package.json";
 
+const appVersion = "my-project-name@" + version;
+
 import * as Sentry from "@sentry/browser";
 Sentry.init({
-  release: "my-project-name@" + version,
+  release: appVersion,
   dsn:
     "https://6e220b18652045c0b402c1845603cf39@o399389.ingest.sentry.io/5256389",
 });
@@ -14,6 +16,8 @@ ReactDOM.render(<div>Hello world!</div>, document.getElementById("root"));
 function foo() {
     throw new Error("testing integration");
 }
+
+console.log(`Version ${appVersion}`);
 
 setTimeout(() => {
     foo();
--- webpack.prod.ts
+++ webpack.prod.ts
@@ -1,10 +1,23 @@
 import { Configuration } from 'webpack';
 import commonConfig from './webpack.common';
 import merge from 'webpack-merge';
+import SentryWebpackPlugin from '@sentry/webpack-plugin';
 
 const prodConfig: Configuration = merge(commonConfig, {
     mode: 'production',
     devtool: 'hidden-source-map',
+    plugins: [
+        new SentryWebpackPlugin(
+            {
+                setCommits: {
+                    repo: "https://github.com/vryonidc/AppMonitoringTest.git",
+                    auto: true
+                },
+                include: '.',
+                ignore: ['node_modules', 'webpack.common.js', 'webpack.dev.js', 'webpack.prod.js'],
+            }
+        )
+    ]
 });
 
 export default prodConfig;
