23ffc031f30f25f16a640e27c6652fbe8dfbfaf6
Constantinos Vryonides <vryonidc@amazon.com>
2020-05-28 11:38:52 +0100

    Fixed version

--- app-html/index.tsx
+++ app-html/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { version } from "../package.json";
 
 import * as Sentry from "@sentry/browser";
 Sentry.init({
-  release: "my-project-name@" + process.env.npm_package_version,
+  release: "my-project-name@" + version,
   dsn:
     "https://6e220b18652045c0b402c1845603cf39@o399389.ingest.sentry.io/5256389",
 });
